7cea981d3aa32b01e9b3611845770d8fe076b0e6
yuvalino <yuvalino@gmail.com>
2024-11-25 00:45:18 +0200

    tvm: added tvm built-in with ps; added comm to tasks

--- main.c
+++ main.c
@@ -55,17 +55,6 @@ int scp_main(int argc, char **argv);
 int _toybox_main(int argc, char **argv);
 void toybox_iter_toy_names(void (*fn)(const char *, void *), void * arg);
 
-// int main(int argc, char **argv)
-// {
-//     tvm_init();
-//     printf("Hello, World!\n");
-//     char *nargv[] = {
-//         argv[0],
-//         NULL
-//     };
-//     return mksh_main(sizeof(nargv)/sizeof(char *) - 1, nargv);
-// }
-
 static void add_toy(const char *toy_name, void *arg)
 {
     char pathname[4096] = {0};
@@ -75,7 +64,7 @@ static void add_toy(const char *toy_name, void *arg)
 
 int main(int argc, char **argv)
 {
-    tvm_init();
+    tvm_init("forkless");
     tvm_register_program("/bin/sh", (main_func_t) mksh_main);
     tvm_register_program("/usr/bin/scp", (main_func_t) scp_main);
     tvm_register_program("/usr/bin/toybox", (main_func_t) _toybox_main);
